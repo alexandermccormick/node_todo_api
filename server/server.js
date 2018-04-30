7b1b59d968ea9d98fda14fe3c75fbdef57393e24
Alexander McCormick <alexandermccormick@protonmail.com>
2018-04-30 13:49:24 -0700

    add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.get("/todos/:id", (req, res) => {
         if (!todo) {
             return res.status(404).send();
         }
-        res.status(200).send(todo);
+        res.status(200).send({todo});
     }, (e) => {
         res.status(404).send();
     });

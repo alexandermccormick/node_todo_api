d88c13b7d3c8d2d45fb5967926e8c42e5e1e4d92
Alexander McCormick <alexandermccormick@protonmail.com>
2018-05-09 17:26:11 -0700

    add POST /users and token method

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("./config/config.js")
+require("./config/config.js");
 
 const _ = require("lodash");
 const express = require("express");
@@ -7,7 +7,7 @@ const {ObjectID} = require("mongodb");
 
 const {mongoose} = require("./db/mongoose.js");
 const {Todo} = require("./models/todo");
-const {USer} = require("./models/user");
+const {User} = require("./models/user");
 
 const app = express();
 const port = process.env.PORT;
@@ -86,6 +86,19 @@ app.patch("/todos/:id", (req, res) => {
         }
         res.send({todo});
     }, (e) => res.status(404).send())
+});
+
+app.post("/users", (req, res) => {
+    let body = _.pick(req.body, ["email", "password"]);
+    let user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header("x-auth", token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(port, () => {
